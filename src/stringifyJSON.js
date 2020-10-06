eb41371b61314e143935c74ea3b964aaf68cbb5f
gordongest <gordon.gest@gmail.com>
2020-10-05 19:21:43 -0500

    stringify complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  result = '';
+
+  if (typeof obj === 'number') {
+    result += obj;
+  } else if (obj === null) {
+    result += 'null';
+  } else if (typeof obj === 'boolean') {
+    result += obj;
+  } else if (typeof obj === 'string') {
+    result += '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    result += '[';
+    for (let i = 0; i < obj.length; i++) {
+      if (i < obj.length - 1) {
+        result += stringifyJSON(obj[i]) + ',';
+      } else {
+        result += stringifyJSON(obj[i]);
+      }
+    }
+    result += ']';
+  } else if (typeof obj === 'object') {
+    let last = Object.keys(obj)[Object.keys(obj).length - 1];
+    result += '{';
+    for (let key in obj) {
+      if (key === 'undefined') {
+        continue;
+      } else if (key === 'functions') {
+        continue;
+      }
+      //if not last element in object, add ','
+      if (key !== last) {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      } else {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      }
+    }
+    result += '}';
+  }
+
+  return result;
 };
